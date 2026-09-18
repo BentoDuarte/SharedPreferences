98c5705b5054ebc585e3057bca371708a259af53
Otavio <tavi.solli2018@gmail.com>
2026-09-18 09:59:41 -0300

    Mudando a logica de salvamento de dados

--- app/src/main/java/com/example/sharedpreferences/database/ProdutoDAO.java
+++ app/src/main/java/com/example/sharedpreferences/database/ProdutoDAO.java
@@ -5,6 +5,8 @@ import android.content.SharedPreferences;
 
 import com.example.sharedpreferences.model.Produto;
 
+import java.util.ArrayList;
+
 public class ProdutoDAO {
 
     private SharedPreferences preferences;
@@ -17,20 +19,68 @@ public class ProdutoDAO {
     // INSERT
     public boolean inserir(Produto produto) {
 
+        // Pega o próximo ID
+        int id = preferences.getInt("ULTIMO_ID", 0) + 1;
+
         SharedPreferences.Editor editor = preferences.edit();
 
-        editor.putString("NOME", produto.getNome());
-        editor.putString("DESCRICAO", produto.getDescricao());
-        editor.putString("PRECO", produto.getPreco().toString());
+        editor.putString("NOME_" + id, produto.getNome());
+        editor.putString("DESCRICAO_" + id, produto.getDescricao());
+        editor.putString("PRECO_" + id, produto.getPreco().toString());
+
+        // Salva o último ID utilizado
+        editor.putInt("ULTIMO_ID", id);
 
         editor.apply();
 
         return true;
     }
 
-    // BUSCA
+    // BUSCA TODOS OS PRODUTOS
+    public ArrayList<Produto> listar() {
+
+        ArrayList<Produto> produtos = new ArrayList<>();
+
+        int ultimoId = preferences.getInt("ULTIMO_ID", 0);
+
+        for (int id = 1; id <= ultimoId; id++) {
+
+            String nome = preferences.getString(
+                    "NOME_" + id,
+                    ""
+            );
+
+            String descricao = preferences.getString(
+                    "DESCRICAO_" + id,
+                    ""
+            );
+
+            String precoString = preferences.getString(
+                    "PRECO_" + id,
+                    "0"
+            );
+
+            if (!nome.isEmpty()) {
+
+                Double preco = Double.parseDouble(precoString);
+
+                Produto produto = new Produto(
+                        preco,
+                        descricao,
+                        nome,
+                        id
+                );
+
+                produtos.add(produto);
+            }
+        }
+
+        return produtos;
+    }
+
+    // BUSCA SE EXISTE ALGUM PRODUTO
     public boolean busca() {
 
-        return preferences.contains("NOME");
+        return preferences.getInt("ULTIMO_ID", 0) > 0;
     }
 }
